0f7c59c55a79cf151dcd991f9ebbfdf2b75f82a8
Ahmad Tariq <ahmad16_tariq@hotmail.com>
2024-12-14 01:18:05 +0300

    renamed:    twoSum.py -> twoSum/twoSum.py     - Add a working solution to Two Sum problem

--- twoSum/twoSum.py
+++ twoSum/twoSum.py
@@ -7,16 +7,21 @@ You can return the answer in any order. '''
 class Solution:
 
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        for index in nums:
-            if  
-        
+        final_list = []
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if ((nums[i] + nums[j]) == target):
+                    final_list.append(i)
+                    final_list.append(j)
+        return final_list
 
 def main():
     nums = [2, 7, 11, 15]
     target = 9
 
     twoSum = Solution()
-    twoSum.twoSum(nums, target)
+    final_list = twoSum.twoSum(nums, target)
+    print(final_list)
 
 if __name__ == '__main__':
     main()
